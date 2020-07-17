7321d5a32c76242cf7f3184ce117e7c19a26af1d
Keesha Hargrove <kharg14@gmail.com>
2020-07-17 01:12:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,9 +63,26 @@ holiday_supplies[:summer][:fourth_of_july][1]
 end
 
 
+def second_supply_for_fourth_of_july(holiday_supplies)
+  holiday_supplies[:summer][:fourth_of_july][1]
+end
+
 second_supply_for_fourth_of_july(holiday_supplies)
 
 
 
+def add_supply_to_winter_holidays(holiday_hash, item)
+ holiday_hash[:winter].each do |holiday, decorations|
+   decorations << item
+  end
+end
 
 
+def add_supply_to_winter_holidays(holiday_hash, item)
+ holiday_hash[:winter].each do |holiday, decorations|
+   decorations.push(item)
+  end
+end
+
+
+add_supply_to_winter_holidays(holiday_supplies, "Baloons")
